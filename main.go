b6901e4894c59a52231d17a2031b5f0a7788b922
Nyxvectar Yan <Nyxvectar@proton.me>
2025-05-31 17:15:56 +0800

    Defined constants and variables

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+/**
+ * Author:  Nyxvectar Yan
+ * Repo:    CipherHive
+ * Created: 05/31/2025
+ */
+
 package main
 
 import "fmt"
@@ -5,3 +11,14 @@ import "fmt"
 func main() {
 	fmt.Printf("Hello, gopher!")
 }
+
+const (
+	batchSize = 10000
+	maxRange  = 1 << 60
+)
+
+var (
+	targetMD5   [16]byte
+	foundChan   = make(chan uint64, 1)
+	globalIndex uint64
+)
